a93c8fa24adf8dfc71855bc1156ee2a82ce9ab61
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-04-10 16:51:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/2trrubmn5qlnw7m2/branch/main?svg=true)](https://ci.appveyor.com/project/Vyacheslav-hub/goblin-games/branch/main)
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // очищает dist перед каждой сборкой
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // уточняем путь к директории
+    },
+    open: true,
+    hot: true, // включаем горячую замену модулей
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // обработка CSS
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource', // обработка изображений
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // используем шаблон index.html из папки src
+      filename: 'index.html', // имя выходного файла
+      inject: 'body', // инжектит скрипты в тело страницы
+    }),
+  ],
+  mode: 'development', // режим разработки
+};
